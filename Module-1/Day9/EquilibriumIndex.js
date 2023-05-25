ae0695343f209c481cc67a8eb96bef11b81f913d
Vijay Kumar <asonv.tech@gmail.com>
2023-05-25 10:47:20 +0530

    prefix sum

---
+++ Module-1/Day9/EquilibriumIndex.js
@@ -0,0 +1,25 @@
+function findEquilibriumIndicesCount(arr) {
+    let count = 0;
+
+    const prefixSum = [];
+    let sum = 0;
+    for (const element of arr) {
+        sum += element;
+        prefixSum.push(sum);
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const leftSum = (i > 0) ? prefixSum[i - 1] : 0;
+        const rightSum = prefixSum[arr.length - 1] - prefixSum[i];
+
+        if (leftSum === rightSum) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const arr = [-3, 2, 4, -1];
+const equilibriumCount = findEquilibriumIndicesCount(arr);
+console.log("Count of equilibrium indices:", equilibriumCount);
